6769dac21e22a99abe452a40387810a914be1bab
José Alencar de Souza Rocha <jose_alencar_1403@hotmail.com>
2017-05-18 15:48:42 -0300

    Add aula 2 - Tela de botões para redirecionamento

---
+++ Layouts/app/src/main/java/br/com/alencar/jose/layouts/BotoesActivity.java
@@ -0,0 +1,30 @@
+package br.com.alencar.jose.layouts;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class BotoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_botoes);
+    }
+
+    public void redirecionarMain(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void redirecionarExercicio2(View view) {
+        Intent i = new Intent(this, Exercicio2Activity.class);
+        startActivity(i);
+    }
+
+    public void redirecionarExercicio3(View view) {
+        Intent i = new Intent(this, Exercicio3Activity.class);
+        startActivity(i);
+    }
+}
