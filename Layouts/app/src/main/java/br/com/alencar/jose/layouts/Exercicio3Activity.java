99b1a13c4a3ae72199c08ff16a10437c2ea58730
José Alencar de Souza Rocha <jose_alencar_1403@hotmail.com>
2017-05-18 16:25:38 -0300

    Add aula 2 - Ajusta renderização da activity pela orientação do dispositivo

--- Layouts/app/src/main/java/br/com/alencar/jose/layouts/Exercicio3Activity.java
+++ Layouts/app/src/main/java/br/com/alencar/jose/layouts/Exercicio3Activity.java
@@ -1,7 +1,10 @@
 package br.com.alencar.jose.layouts;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Exercicio3Activity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Exercicio3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercicio3);
+        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBotton);
+        int orientation = getResources().getConfiguration().orientation;
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            llBottom.setVisibility(View.GONE);
+        } else {
+            llBottom.setVisibility(View.VISIBLE);
+        }
     }
 }
