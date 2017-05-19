3fd5602ea3a61deda4d1badc4a758cc9923a89a1
José Alencar de Souza Rocha <jose_alencar_1403@hotmail.com>
2017-05-19 14:44:29 -0300

    Add aula 3 - Ajuste de validações no formulário de login

--- Layouts/app/src/main/java/br/com/alencar/jose/layouts/RelativoActivity.java
+++ Layouts/app/src/main/java/br/com/alencar/jose/layouts/RelativoActivity.java
@@ -21,53 +21,79 @@ public class RelativoActivity extends AppCompatActivity {
         "teste@teste.com:123"
     );
 
+    private TextView tvUsuarioIncorreto;
+    private TextView tvEmptyEmail;
+    private TextView tvEmptyPassword;
+    private TextView tvCredenciaisInvalidas;
+    private EditText etEmail;
+    private EditText etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relativo);
+
+        tvUsuarioIncorreto = (TextView) findViewById(R.id.tvUsuarioIncorreto);
+        tvEmptyEmail = (TextView) findViewById(R.id.tvEmptyEmail);
+        tvEmptyPassword = (TextView) findViewById(R.id.tvEmptyPassword);
+        tvCredenciaisInvalidas = (TextView) findViewById(R.id.tvCredenciaisInvalidas);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+
+        etEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    validaCampoEmail(v);
+                }
+            }
+        });
+
+        etPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    validaCampoPassword(v);
+                }
+            }
+        });
     }
 
     public void logar(View view) {
-        TextView tvUsuarioIncorreto = (TextView) findViewById(R.id.tvUsuarioIncorreto);
-        TextView tvEmptyEmail = (TextView) findViewById(R.id.tvEmptyEmail);
-        TextView tvEmptyPassword = (TextView) findViewById(R.id.tvEmptyPassword);
         tvUsuarioIncorreto.setVisibility(View.GONE);
         tvEmptyEmail.setVisibility(View.GONE);
         tvEmptyPassword.setVisibility(View.GONE);
-        EditText etEmail = (EditText) findViewById(R.id.etEmail);
-        EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        tvCredenciaisInvalidas.setVisibility(View.GONE);
 
         String txtEmail = etEmail.getText().toString();
         String txtPassword = etPassword.getText().toString();
 
-        boolean isEmailVazio = txtEmail.isEmpty();
-        boolean isPasswordVazia = txtPassword.isEmpty();
-        boolean isEmailValido = android.util.Patterns.EMAIL_ADDRESS.matcher(txtEmail).matches();
-
-        if (isEmailVazio) {
-            tvEmptyEmail.setVisibility(View.VISIBLE);
-            setFocus(etEmail);
-        }
-
-        if (isPasswordVazia) {
+        if (isCampoVazio(txtPassword)) {
             tvEmptyPassword.setVisibility(View.VISIBLE);
             setFocus(etPassword);
         }
 
-        if (!isEmailValido) {
-            tvUsuarioIncorreto.setVisibility(View.VISIBLE);
+        if (isCampoVazio(txtEmail)) {
+            tvEmptyEmail.setVisibility(View.VISIBLE);
             setFocus(etEmail);
+            return;
         }
 
-        if (!isEmailVazio && !isPasswordVazia && isEmailValido) {
-            for (String credencial : credenciais) {
-                if (credencial.equals(txtEmail + ":" + txtPassword)) {
-                    Intent i = new Intent(this, UsuarioActivity.class);
-                    i.putExtra("usuario", txtEmail);
-                    startActivity(i);
-                }
+        if (!isEmailValido(txtEmail)) {
+            tvUsuarioIncorreto.setVisibility(View.VISIBLE);
+            setFocus(etEmail);
+            return;
+        }
+
+        for (String credencial : credenciais) {
+            if (credencial.equals(txtEmail + ":" + txtPassword)) {
+                Intent i = new Intent(this, UsuarioActivity.class);
+                i.putExtra("usuario", txtEmail);
+                startActivity(i);
+                return;
             }
         }
+        tvCredenciaisInvalidas.setVisibility(View.VISIBLE);
 
     }
 
@@ -75,5 +101,39 @@ public class RelativoActivity extends AppCompatActivity {
         view.requestFocus();
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    public void validaCampoEmail(View view) {
+        String txtEmail = etEmail.getText().toString();
+        tvUsuarioIncorreto.setVisibility(View.GONE);
+        if (isCampoVazio(txtEmail)) {
+            tvEmptyEmail.setVisibility(View.VISIBLE);
+            return;
+        } else {
+            tvEmptyEmail.setVisibility(View.GONE);
+        }
+
+        if (isEmailValido(txtEmail)) {
+            tvUsuarioIncorreto.setVisibility(View.GONE);
+        } else {
+            tvUsuarioIncorreto.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void validaCampoPassword(View view) {
+        String txtPassword = etPassword.getText().toString();
+        if (isCampoVazio(txtPassword)) {
+            tvEmptyPassword.setVisibility(View.VISIBLE);
+        } else {
+            tvEmptyPassword.setVisibility(View.GONE);
+        }
+    }
+
+    private boolean isCampoVazio(String campo) {
+        return campo.isEmpty();
+    }
+
+    private boolean isEmailValido(String email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
